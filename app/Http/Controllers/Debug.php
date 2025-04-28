56f5610e0447101e57854eab2a94724977e48c4a
mark19992 <maturanmark1999@gmail.com>
2025-04-28 14:46:59 +0800

    ReturnhomeUpdate

--- app/Http/Controllers/Debug.php
+++ app/Http/Controllers/Debug.php
@@ -8,7 +8,7 @@ class Debug extends Controller
 {
     public function index () {
 
-        return view('debug.homepage');
+        return view('debug.returnhome');
     }
    
 }
---
+++ resources/views/debug/returnhome.blade.php
@@ -0,0 +1,14 @@
+<x-basecomponent>
+<x-pagecomponent asset='assets/Cantilan/agila white Beach/IMG_1312.JPG' nextLink="#" buttonText="Return Home">
+    
+    <div class="d-flex flex-row justify-content-start align-items-center">
+        <img src="{{ asset('assets/image/Logo/lolo1.png') }}" alt="" style="width: 100%; height: 100%;">
+    </div>
+
+    <div class="fw-bold fs-5 justify-content-center my-3">
+        <h5 class="my-2 fw-bold fs-5 text-center ">Reserved</h5>
+        <h5 class="my-2 fw-bold fs-5 text-center ">Successfully!</h5>
+    </div>
+
+</x-pagecomponent>
+</x-basecomponent>
