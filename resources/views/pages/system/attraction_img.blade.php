e5799d7fac297580af8bc88cd55ff75b879e9f36
LydianJay <lloydjayedradan@gmail.com>
2025-05-25 16:23:53 +0800

    awdawdaw

--- resources/views/pages/system/attraction_img.blade.php
+++ resources/views/pages/system/attraction_img.blade.php
@@ -46,7 +46,9 @@
                             <a href="{{ route('delete_img_attraction', ['id' => $a->id]) }}">
                                 <i class="bi bi-x fs-1"></i>
                             </a>
+
                         </td>
+                       
                     </tr>
                 @endforeach
             </tbody>
